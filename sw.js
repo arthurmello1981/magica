9b1c7fd5f9fcaca54ad36c4d1296f4fcce115966
arthurmello1981 <arthur@vitainvestimentos.com>
2026-06-23 07:33:05 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const C="cal-v1";
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./icon.png"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate",e=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
